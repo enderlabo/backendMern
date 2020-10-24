04b2386cc13bc217c7d143ad3046caf194365684
EnderLab <enderlabo@gmail.com>
2020-10-24 18:25:37 -0500

    controller/auth and middlewares/validateJWT updated

--- controller/auth.js
+++ controller/auth.js
@@ -1,120 +1,109 @@
-const { response } = require('express');
-const bcrypt = require('bcryptjs');
-const User = require('../models/userModel');
-const { generateJWT } = require('../helpers/JWT');
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const { generateJWT } = require("../helpers/JWT");
 
-const createUser = async ( req, res = response ) => {
-   
-   const { email, password } = req.body;
+const createUser = async (req, res = response) => {
+  const { email, password } = req.body;
 
-   //If email exists return status400
-   try {
-       let user = await User.findOne({ email });
+  //If email exists return status400
+  try {
+    let user = await User.findOne({ email });
 
-        if( user ) {
-
-            return res.status(400).json({
-               ok: false,
-               msg: 'Email already is used'
-           });
-        }
-
-        user = new User( req.body );
-        
-        //Password Encrpt
-        const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync( password, salt );
-
-        await user.save();
-        //Generate JWT
-        const token = await generateJWT( user.id, user.name );
-
-        res.status(201).json({
-            ok: true,
-            uid: user.id,
-            name: user.name,
-            token
-        });
-
-
-} catch ( error ) {
-        console.log( error );
-        res.status(400).json({
-            ok: false,
-            msg: 'Bad request, contact to Admin'
-        })
-    }
-}
-
-const loginUser = async ( req, res = response ) => {
-
-    const { email, password } = req.body;
-    
-    try {
-        //If email exists return status400
-        var user = await User.findOne({ email });
-
-        if( !user ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'The user does not match with that email.'
-            })
-        }
-        //Match password with bycrept
-        const validPassword = bcrypt.compareSync( password, user.password );
-
-        if( !validPassword ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'The password is not valid.'
-            })
-        }
-        
-        //Generate JWT
-        const token = await generateJWT( user.id, user.name );
-
-        res.status(200).json({
-            ok: true,
-            uid: user.id,
-            name: user.name,
-            token
-        })
-
-    } catch( err ) {
-        console.log( err );
-        res.status(500).json({
-            ok: false,
-            msg: 'Bad request, contact to Admin'
-        })
-
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Email already is used",
+      });
     }
 
+    user = new User(req.body);
 
+    //Password Encrpt
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
 
-    // res.status(200).json({
-    //     ok: true,
-    //     msg: 'Login',
-    //     email,
-    //     token
-    // })
-}
+    await user.save();
+    //Generate JWT
+    const token = await generateJWT(user.id, user.name);
 
-const renewToken = async ( req, res = response ) => {
-
-    //Generate New Token
-    const token = await jwt.generateJWT( req.uid, req.name );
-
-    
-    res.status(200).json({
-        ok: true,
-        token
-        
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      token,
     });
-}
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: "Bad request, contact to Admin",
+    });
+  }
+};
 
+const loginUser = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    //If email exists return status400
+    var user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The user does not match with that email.",
+      });
+    }
+    //Match password with bycrept
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The password is not valid.",
+      });
+    }
+
+    //Generate JWT
+    const token = await generateJWT(user.id, user.name);
+
+    res.status(200).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      ok: false,
+      msg: "Bad request, contact to Admin",
+    });
+  }
+
+  // res.status(200).json({
+  //     ok: true,
+  //     msg: 'Login',
+  //     email,
+  //     token
+  // })
+};
+
+const renewToken = async (req, res = response) => {
+  //Generate New Token
+  const token = await jwt.generateJWT(req.uid, req.name);
+
+  res.status(200).json({
+    ok: true,
+    id: req.uid,
+    name: req.name,
+    token,
+  });
+};
 
 module.exports = {
-    createUser,
-    loginUser,
-    renewToken
-}
+  createUser,
+  loginUser,
+  renewToken,
+};
--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,42 +1,33 @@
 const { response } = require("express");
 const { JsonWebTokenError } = require("jsonwebtoken");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+const checkJWT = (req, res = response, next) => {
+  //x-token - headers
+  const token = req.header("x-token");
+  console.log(token);
+  if (!token) {
+    return res.status(400).json({
+      ok: false,
+      msg: "There is no token in the request.",
+    });
+  }
 
-const checkJWT = ( req, res = response, next ) => {
+  try {
+    const { name, uid } = jwt.verify(token, process.env.SECRET_JWT);
+    //data taken from request
+    req.uid = uid;
+    req.name = name;
+  } catch (err) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token is not valid",
+    });
+  }
 
-    //x-token - headers
-    const token = req.header('x-token');
-    console.log(token);
-    if( !token ) {
-        return res.status(400).json({
-            ok: false,
-            msg: 'There is no token in the request.'
-        })
-    }
-
-    try {
-    
-        const { name, uid } = jwt.verify(
-            token,
-            process.env.SECRET_JWT
-        );
-        //data taken from request
-        req.uid = uid;
-        req.name = name;
-        
-    } catch (err) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token is not valid'
-        })
-    }
-
-    next();
-
-}
-
+  next();
+};
 
 module.exports = {
-    checkJWT
-}
+  checkJWT,
+};
